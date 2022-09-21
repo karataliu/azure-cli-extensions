5cd96abc64fdd5ff0a397b50dd19186ee63017a6
Dong Liu <doliu@microsoft.com>
2022-09-21 16:08:29 +0800

    fixup! Rotate sa key

--- src/aks-preview/azext_aks_preview/custom.py
+++ src/aks-preview/azext_aks_preview/custom.py
@@ -1669,6 +1669,8 @@ def aks_enable_addons(cmd, client, resource_group_name, name, addons, workspace_
 def aks_rotate_certs(cmd, client, resource_group_name, name, no_wait=True):     # pylint: disable=unused-argument
     return sdk_no_wait(no_wait, client.begin_rotate_cluster_certificates, resource_group_name, name)
 
+def aks_rotate_service_account_signing_keys(cmd, client, resource_group_name, name, no_wait=True):     # pylint: disable=unused-argument
+    pass
 
 def _update_addons(cmd,  # pylint: disable=too-many-branches,too-many-statements
                    instance,
